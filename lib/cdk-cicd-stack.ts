31e6d073f39475b5dd54ae1d4c94390ef594ca6e
Liju Mathew <liju.mathew@intimetec.com>
2024-10-03 11:43:17 -0600

    AWS Codepipeline Practice

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,19 @@ export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'MyfirstPipeline', {
+      pipelineName: 'MyfirstPipeline',
+      synth: new ShellStep('Synth', {
+       input: CodePipelineSource.gitHub('alexhddev/CDK-course-resources', 'cicd-practice'),
+       commands: [
+        'cd cdk-cicd',
+        'npm ci',
+        'npx cdk synth'
+      ],
+      primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+        
+    })
+    
   }
 }
